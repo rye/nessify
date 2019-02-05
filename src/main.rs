30f3d72da32c272cd6b8791d2ecfc708299b93bd
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-05 08:10:49 -0600

    clap: Accept dump arguments
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
@@ -8,5 +8,17 @@ fn main() {
 	let matches = App::new("nessify")
 		.version(VERSION)
 		.author(AUTHORS)
+		.arg(Arg::with_name("dump")
+		     .multiple(true)
+		     .required(true))
 		.get_matches();
+
+	match matches.values_of("dump") {
+		Some(results) => {
+			for dump in results {
+				println!("Dumps: {:?}", dump);
+			}
+		},
+		None => panic!("No dump filenames given; cannot do anything.")
+	}
 }
