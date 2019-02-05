1160c74811b6aba32018b2f80d9f5378054e3327
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-05 08:10:14 -0600

    main: Set up metadata for clap to render
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 extern crate clap;
 use clap::App;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const AUTHORS: &'static str = env!("CARGO_PKG_AUTHORS");
+
 fn main() {
 	let matches = App::new("nessify")
+		.version(VERSION)
+		.author(AUTHORS)
 		.get_matches();
 }
