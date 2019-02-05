3b37c933a23ba82cf8b697850402c7e287009218
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-05 08:12:34 -0600

    main: Add `config` dummy argument
    Logic to be implemented at a later date.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,21 @@ fn main() {
 	let matches = App::new("nessify")
 		.version(VERSION)
 		.author(AUTHORS)
+		.arg(Arg::with_name("config")
+		     .value_name("FILE")
+		     .short("c")
+		     .long("config")
+		     .help("Sets a configuration file to load before applying arguments")
+		     .takes_value(true))
 		.arg(Arg::with_name("dump")
 		     .value_name("DUMP")
 		     .multiple(true)
 		     .required(true))
 		.get_matches();
+
+	let config = matches.value_of("config")
+		.unwrap_or("config.yml");
+	println!("Configuration: {}", config);
 
 	match matches.values_of("dump") {
 		Some(results) => {
