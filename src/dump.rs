db0953341fe3094553b920ce70674d6ff737c3b5
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-06 15:31:33 -0600

    dump: Move Record struct into its own file
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/dump.rs
+++ src/dump.rs
@@ -2,39 +2,7 @@ use std::collections::{hash_map::DefaultHasher, HashSet};
 use std::hash::{Hash, Hasher};
 use std::net::IpAddr;
 
-// {Host, Protocol, Port} => Host
-// {Plugin ID, CVE, CVSS, Name, Synopsis, Description, Solution, See Also} => Plugin
-// {Plugin Output} => Detection (&Host, &Plugin) also
-
-#[derive(Debug, Deserialize)]
-struct Record {
-	#[serde(rename = "Plugin ID")]
-	plugin_id: i32,
-	#[serde(rename = "CVE")]
-	cve: String,
-	#[serde(rename = "CVSS")]
-	cvss: String,
-	#[serde(rename = "Risk")]
-	risk: String,
-	#[serde(rename = "Host")]
-	host: String,
-	#[serde(rename = "Protocol")]
-	protocol: String,
-	#[serde(rename = "Port")]
-	port: u32,
-	#[serde(rename = "Name")]
-	name: String,
-	#[serde(rename = "Synopsis")]
-	synopsis: String,
-	#[serde(rename = "Description")]
-	description: String,
-	#[serde(rename = "Solution")]
-	solution: String,
-	#[serde(rename = "See Also")]
-	see_also: String,
-	#[serde(rename = "Plugin Output")]
-	plugin_output: String,
-}
+mod record;
 
 #[derive(Clone, Eq)]
 struct Plugin {
@@ -86,6 +54,7 @@ struct Detection {
 	plugin: Plugin,
 	plugin_output: String,
 }
+use record::*;
 
 #[allow(dead_code)]
 pub struct Dump {
