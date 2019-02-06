33eb946dff3ce5c43df48dc51a40b61125e3a6c0
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-06 14:10:45 -0600

    Record: Add fields
    These correspond one-to-one with the fields in a Nessus CSV file.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/dump.rs
+++ src/dump.rs
@@ -3,6 +3,19 @@
 // {Plugin Output} => Detection (&Host, &Plugin) also
 
 struct Record {
+	plugin_id: i32,
+	cve: String,
+	cvss: String,
+	risk: String,
+	host: String,
+	protocol: String,
+	port: u32,
+	name: String,
+	synopsis: String,
+	description: String,
+	solution: String,
+	see_also: String,
+	plugin_output: String,
 }
 
 struct Plugin {
