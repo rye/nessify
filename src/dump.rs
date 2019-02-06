b31a9319b19c95d78343d0f83365ade2c812be54
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-06 12:56:37 -0600

    dump: Add five structs
    To get started, we'll use these.
    
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/dump.rs
+++ src/dump.rs
@@ -1,3 +1,18 @@
 // {Host, Protocol, Port} => Host
 // {Plugin ID, CVE, CVSS, Name, Synopsis, Description, Solution, See Also} => Plugin
 // {Plugin Output} => Detection (&Host, &Plugin) also
+
+struct Record {
+}
+
+struct Plugin {
+}
+
+struct Host {
+}
+
+struct Detection {
+}
+
+pub struct Dump {
+}
