d0c284cc51a40f3b26627a3979065a48ec056434
Kristofer Rye <kristofer.rye@gmail.com>
2019-02-06 14:11:42 -0600

    Record: Add serde mappings between field headers and fields
    Signed-off-by: Kristofer Rye <kristofer.rye@gmail.com>
    Tested-by: Kristofer Rye <kristofer.rye@gmail.com>

--- src/dump.rs
+++ src/dump.rs
@@ -4,18 +4,31 @@
 
 #[derive(Debug, Deserialize)]
 struct Record {
+	#[serde(rename = "Plugin ID")]
 	plugin_id: i32,
+	#[serde(rename = "CVE")]
 	cve: String,
+	#[serde(rename = "CVSS")]
 	cvss: String,
+	#[serde(rename = "Risk")]
 	risk: String,
+	#[serde(rename = "Host")]
 	host: String,
+	#[serde(rename = "Protocol")]
 	protocol: String,
+	#[serde(rename = "Port")]
 	port: u32,
+	#[serde(rename = "Name")]
 	name: String,
+	#[serde(rename = "Synopsis")]
 	synopsis: String,
+	#[serde(rename = "Description")]
 	description: String,
+	#[serde(rename = "Solution")]
 	solution: String,
+	#[serde(rename = "See Also")]
 	see_also: String,
+	#[serde(rename = "Plugin Output")]
 	plugin_output: String,
 }
 
